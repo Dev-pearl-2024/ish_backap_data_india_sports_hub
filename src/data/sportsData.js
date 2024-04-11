b175cea45db235af87e41a9ee31834d156b8d19c
Raman Kumar <raman@parentune.com>
2024-04-12 00:42:30 +0530

    added icons for each sports

---
+++ src/data/sportsData.js
@@ -0,0 +1,149 @@
+import TennisIcon from '../assets/icons/sportIcons/tennis.svg';
+import Wrestling from '../assets/icons/sportIcons/Wrestling.svg';
+import Sailing from '../assets/icons/sportIcons/Sailing.svg';
+import Swimming from '../assets/icons/sportIcons/Swimming.svg';
+import Judo from '../assets/icons/sportIcons/Judo.svg';
+import TableTennis from '../assets/icons/sportIcons/Tabletennis.svg';
+import Shooting from '../assets/icons/sportIcons/Shooting.svg';
+import Golf from '../assets/icons/sportIcons/Golf.svg';
+import Fencing from '../assets/icons/sportIcons/Fencing.svg';
+import Gymnastic from '../assets/icons/sportIcons/Gymnastic.svg';
+import Athletics from '../assets/icons/sportIcons/Athletics.svg';
+import Badminton from '../assets/icons/sportIcons/Badminton.svg';
+import Kayak from '../assets/icons/sportIcons/Kayak.svg';
+import Equestrian from '../assets/icons/sportIcons/Equestrian.svg';
+import Hocky from '../assets/icons/sportIcons/Hockey.svg';
+import Handball from '../assets/icons/sportIcons/Handball.svg';
+import Basketball from '../assets/icons/sportIcons/Basketball.svg';
+import Boxing from '../assets/icons/sportIcons/Boxing.svg';
+import Football from '../assets/icons/sportIcons/Football.svg';
+import Wallyball from '../assets/icons/sportIcons/Wallyball.svg';
+import Archery from '../assets/icons/sportIcons/Archery.svg';
+import Weightlifting from '../assets/icons/sportIcons/Weightlifting.svg';
+import Taekwondo from '../assets/icons/sportIcons/Taekwondo.svg';
+import Cycling from '../assets/icons/sportIcons/Cycling.svg';
+
+const sportsData = [
+  {
+    name: 'Tennis',
+    icon: <TennisIcon />,
+    status: 'inactive',
+  },
+  {
+    name: 'Wrestling',
+    icon: <Wrestling />,
+    status: 'active',
+  },
+  {
+    name: 'Sailing',
+    icon: <Sailing />,
+    status: 'active',
+  },
+  {
+    name: 'Swimming',
+    icon: <Swimming />,
+    status: 'inactive',
+  },
+  {
+    name: 'Judo',
+    icon: <Judo />,
+    status: 'inactive',
+  },
+  {
+    name: 'Table Tennis',
+    icon: <TableTennis />,
+    status: 'inactive',
+  },
+  {
+    name: 'Shooting',
+    icon: <Shooting />,
+    status: 'inactive',
+  },
+  {
+    name: 'Golf',
+    icon: <Golf />,
+    status: 'inactive',
+  },
+  {
+    name: 'Fencing',
+    icon: <Fencing />,
+    status: 'inactive',
+  },
+  {
+    name: 'Gymnastic',
+    icon: <Gymnastic />,
+    status: 'inactive',
+  },
+  {
+    name: 'Athletics',
+    icon: <Athletics />,
+    status: 'inactive',
+  },
+  {
+    name: 'Badminton',
+    icon: <Badminton />,
+    status: 'inactive',
+  },
+  {
+    name: 'Kayak',
+    icon: <Kayak />,
+    status: 'inactive',
+  },
+  {
+    name: 'Equestrian',
+    icon: <Equestrian />,
+    status: 'inactive',
+  },
+  {
+    name: 'Hocky',
+    icon: <Hocky />,
+    status: 'inactive',
+  },
+  {
+    name: 'Handball',
+    icon: <Handball />,
+    status: 'inactive',
+  },
+  {
+    name: 'Basketball',
+    icon: <Basketball />,
+    status: 'inactive',
+  },
+  {
+    name: 'Boxing',
+    icon: <Boxing />,
+    status: 'inactive',
+  },
+  {
+    name: 'Football',
+    icon: <Football />,
+    status: 'inactive',
+  },
+  {
+    name: 'Wallyball',
+    icon: <Wallyball />,
+    status: 'inactive',
+  },
+  {
+    name: 'Archery',
+    icon: <Archery />,
+    status: 'inactive',
+  },
+  {
+    name: 'Weightlifting',
+    icon: <Weightlifting />,
+    status: 'inactive',
+  },
+  {
+    name: 'Taekwondo',
+    icon: <Taekwondo />,
+    status: 'inactive',
+  },
+  {
+    name: 'Cycling',
+    icon: <Cycling />,
+    status: 'inactive',
+  },
+];
+
+export default sportsData;
