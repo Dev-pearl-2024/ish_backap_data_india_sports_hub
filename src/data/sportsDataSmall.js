ca2e511c6a8648ee33b443b2510d237099052301
Gautam Swami <gautamswami@UPLAP1461.local>
2024-05-29 00:08:16 +0530

    updates

---
+++ src/data/sportsDataSmall.js
@@ -0,0 +1,149 @@
+import TennisIcon from '../assets/icons/sportIcons/tennis.svg';
+import Wrestling from '../assets/icons/sportIcons/Wrestling.svg';
+import Sailing from '../assets/icons/sportIcons/Sailing.svg';
+import Swimming from '../assets/icons/sportIcons/Swimming.svg';
+import Judo from '../assets/icons/sportIcons/Judo.svg';
+import TableTennis from '../assets/icons/sportIcons/Tabletennis.svg';
+import Shooting from '../assets/icons/sportIcons/Shooting.svg';
+import Golf from '../assets/icons/sportIcons/Golf.svg';
+import Fencing from '../assets/icons/sportIcons/Fencing.svg';
+import Gymnastic from '../assets/icons/sportIcons/Gymnastic.svg';
+import Athletics from '../assets/icons/sportIcons/Athletics.svg';
+import Badminton from '../assets/icons/sportIcons/Badminton.svg';
+import Kayak from '../assets/icons/sportIcons/Kayak.svg';
+import Equestrian from '../assets/icons/sportIcons/Equestrian.svg';
+import Hocky from '../assets/icons/sportIcons/Hockey.svg';
+import Handball from '../assets/icons/sportIcons/Handball.svg';
+import Basketball from '../assets/icons/sportIcons/Basketball.svg';
+import Boxing from '../assets/icons/sportIcons/Boxing.svg';
+import Football from '../assets/icons/sportIcons/Football.svg';
+import Wallyball from '../assets/icons/sportIcons/Wallyball.svg';
+import Archery from '../assets/icons/sportIcons/Archery.svg';
+import Weightlifting from '../assets/icons/sportIcons/Weightlifting.svg';
+import Taekwondo from '../assets/icons/sportIcons/Taekwondo.svg';
+import Cycling from '../assets/icons/sportIcons/Cycling.svg';
+
+const iconData = [
+  {
+    name: 'Tennis',
+    icon: <TennisIcon style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Wrestling',
+    icon: <Wrestling style={{width: 10, height: 10}} />,
+    status: 'active',
+  },
+  {
+    name: 'Sailing',
+    icon: <Sailing style={{width: 10, height: 10}} />,
+    status: 'active',
+  },
+  {
+    name: 'Swimming',
+    icon: <Swimming style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Judo',
+    icon: <Judo style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'TableTennis',
+    icon: <TableTennis style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Shooting',
+    icon: <Shooting style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Golf',
+    icon: <Golf style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Fencing',
+    icon: <Fencing style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Gymnastics',
+    icon: <Gymnastic style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Athletics',
+    icon: <Athletics style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Badminton',
+    icon: <Badminton style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Kayaking',
+    icon: <Kayak style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Equestrian',
+    icon: <Equestrian style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Hockey',
+    icon: <Hocky style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Handball',
+    icon: <Handball style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Basketball',
+    icon: <Basketball style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Boxing',
+    icon: <Boxing style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Football',
+    icon: <Football style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Volleyball',
+    icon: <Wallyball style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Archery',
+    icon: <Archery style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Weightlifting',
+    icon: <Weightlifting style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Taekwondo',
+    icon: <Taekwondo style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+  {
+    name: 'Cycling',
+    icon: <Cycling style={{width: 10, height: 10}} />,
+    status: 'inactive',
+  },
+];
+
+export default iconData;
